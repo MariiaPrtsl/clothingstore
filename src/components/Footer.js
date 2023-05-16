f37ae84482166b7a038892cd4096e097aee54bae
Mariia Protsailo <108431414+MariiaPrtsl@users.noreply.github.com>
2023-05-16 12:45:48 +0300

    add

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-300 py-12">
+      <div className="container mx-auto">
+        <p className="text-slate-400 text-center">
+          Coryping &copy; Ecommerce Shop 2023. All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
